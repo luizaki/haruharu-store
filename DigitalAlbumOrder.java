3138c303694fa6ba14eaf329a7495a2467188e27
luizaki <francinelouise.sanchez@gmail.com>
2023-11-18 22:34:42 +0800

    Fixed missing display attributes

--- DigitalAlbumOrder.java
+++ DigitalAlbumOrder.java
@@ -28,6 +28,10 @@ public class DigitalAlbumOrder extends AlbumOrder{
     }
 
     public void display(){
+        String discount;
+        if(discounted) discount = "Yes";
+        else discount = "No";
+
         System.out.println("[Displaying Order Ref. " + refID + "]");
         System.out.println("\tAlbum: " + album.getAlName() + " (" + album.getRelease() + ")");
         System.out.println("\tArtist: " + album.getArtist() + " (" + album.getArtistType() + ")");
@@ -39,7 +43,7 @@ public class DigitalAlbumOrder extends AlbumOrder{
         System.out.println("\tBuyer Email: " + this.buyerEmail);
         System.out.println("\tSubtotal: " + phpFormat.format(this.subTotal));
         System.out.println();
-        System.out.println("\tHas 10% Discount: " + this.discounted);
+        System.out.println("\tHas 10% Discount: " + discount);
         System.out.println("\tTotal Price: " + phpFormat.format(this.totalPrice));        
         System.out.println();
         System.out.println("\tDate of Purchase: " + this.datePurchased);
